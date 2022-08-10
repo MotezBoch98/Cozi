de1ad50795edfed87a65fe4ccfb1f598788dff2e
Motez Bouchhioua <motez.bouchhioua@esprit.tn>
2022-08-10 09:48:58 +0200

    initial commit

---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="sidebar" data-color="orange">
+  <!--
+    Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+-->
+  <div class="sidebar-wrapper" id="sidebar-wrapper">
+    <ul class="nav">
+      <li class="@if ($activePage == 'home') active @endif">
+        <a href="{{ route('home') }}">
+          <i class="now-ui-icons design_app"></i>
+          <p>{{ __('Dashboard') }}</p>
+        </a>
+      </li>
+      <li>
+        <div class="collapse show" id="laravelExamples">
+          <ul class="nav">
+            <li class="@if ($activePage == 'profile') active @endif">
+              <a href="{{ route('profile.edit') }}">
+                <i class="now-ui-icons users_single-02"></i>
+                <p> {{ __("User Profile") }} </p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      <li class="@if ($activePage == 'icons') active @endif">
+        <a href="{{ route('page.index','icons') }}">
+          <i class="now-ui-icons education_atom"></i>
+          <p>{{ __('Icons') }}</p>
+        </a>
+      </li>
+      <li class = " @if ($activePage == 'notifications') active @endif">
+        <a href="{{ route('page.index','notifications') }}">
+          <i class="now-ui-icons ui-1_bell-53"></i>
+          <p>{{ __('Notifications') }}</p>
+        </a>
+      </li>
+      </li>
+    </ul>
+  </div>
+</div>
